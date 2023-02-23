c529515772bb668d1342bbe622be8669418e77ab
mhamidjamil <m.hamid.jamil99@gmail.com>
2023-02-23 18:07:05 +0500

    before_lecture_29

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,22 @@
 class TodosController < ApplicationController
+
     def new
         @todo = Todo.new
     end
+
+    def create
+        @todo = Todo.create(todo_params)
+        if @todo.save
+            redirect_to todo_path(@todo)
+        else
+            render 'new'
+        end
+    end 
+
+    private
+
+        def todo_params
+            params.require(:todo).permit(:name, :description)
+        end
+        
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_063153) do
   create_table "todos", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
