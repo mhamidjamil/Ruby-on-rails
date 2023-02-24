d79aacc33c3083aa572ead2d09bc61a037e6e328
mhamidjamil <m.hamid.jamil99@gmail.com>
2023-02-24 12:52:39 +0500

    Lecture_37

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -32,6 +32,11 @@ class TodosController < ApplicationController
             render 'edit'
         end
     end
+
+    def index
+        @todos = Todo.all
+    end
+
     private
 
         def todo_params
