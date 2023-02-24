2f34ea0433eb456d9b1921c98318bc3fd3ac9628
mhamidjamil <m.hamid.jamil99@gmail.com>
2023-02-24 17:24:34 +0500

    git_code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,46 +1,53 @@
 class TodosController < ApplicationController
-
-    def new
-        @todo = Todo.new
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
+  
+  def new
+    @todo = Todo.new
+  end
+  
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      flash[:notice] = "Todo was created successfully!"
+      redirect_to todo_path(@todo)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was successfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
+  end
+  
+  def index
+    @todos = Todo.all
+  end
+  
+  def destroy
+    @todo.destroy
+    flash[:notice] = "Todo was deleted successfully"
+    redirect_to todos_path
+  end
+  
+  private
+  
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+  
+    def todo_params
+      params.require(:todo).permit(:name, :description)
     end
 
-    def create
-        @todo = Todo.create(todo_params)
-        if @todo.save
-            flash[:notice] = "Todo was created successfully"
-            redirect_to todo_path(@todo)
-        else
-            render 'new'
-        end
-    end 
-
-    def show
-        @todo = Todo.find(params[:id])
-    end    
-    
-    def edit
-        @todo = Todo.find(params[:id])
-    end
-    
-    def update
-        @todo = Todo.find(params[:id])
-        if @todo.update(todo_params)
-            flash[:notice] = "Todo was updated successfully"
-            redirect_to todo_path @todo
-
-        else
-            render 'edit'
-        end
-    end
-
-    def index
-        @todos = Todo.all
-    end
-
-    private
-
-        def todo_params
-            params.require(:todo).permit(:name, :description)
-        end
-        
 end
